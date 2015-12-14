5164fb18d3674fdb65b197f3a4b5c30b25a39f63
Peter Wang <wangwangzw@126.com>
2015-12-14 22:12:25 +0800

    Add project premake config file.

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+-- premake5.lua
+
+workspace "HelloWorld"
+    configurations { "Debug", "Release" }
+
+project "HelloWorld"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files { "**.h", "**.cpp" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        flags { "Symbols" }
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
